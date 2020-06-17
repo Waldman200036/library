7162508b3eb48f334fd29bca3f71e3f5e61c2c9c
Waldman200036 <Waldman200036@gmail.com>
2020-06-16 21:16:33 -0500

    ESLint Changes and Updates

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-/*jshint esversion: 6 */
+/* jshint esversion: 6 */
 import express from 'express';
 import chalk from 'chalk';
 import debug from 'debug';
 import morgan from 'morgan';
 import p from 'path';
 
-var app = express();
-var dbug = debug('app');
-var path = p;
-var __dirname = path.resolve();
+const app = express();
+const dbug = debug('app');
+const path = p;
+const dirname = path.resolve();
 
 app.use(morgan('tiny'));
-app.use(express.static(path.join(__dirname,'/public')));
-app.use('/css', express.static(path.join(__dirname,'/node_modules/bootstrap/dist/css')));
-app.use('/js', express.static(path.join(__dirname,'/node_modules/bootstrap/dist/js')));
-app.use('/js', express.static(path.join(__dirname,'/node_modules/bootstrap/jqery/dist')));
+app.use(express.static(path.join(dirname, '/public')));
+app.use('/css', express.static(path.join(dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(dirname, '/node_modules/bootstrap/jqery/dist')));
 
-app.get('/',function(req, res){
-    res.sendFile(path.join(__dirname,'views/index.html'));
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(dirname, 'views/index.html'));
 });
 
-app.listen(3000,function(){
-    dbug(`listening on port ${chalk.green('3000')}`);
+app.listen(3000, () => {
+  dbug(`listening on port ${chalk.green('3000')}`);
 });
