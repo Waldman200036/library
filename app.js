73e8454c5405ef51b9ff44a5f2ff5e02d945f20b
Waldman200036 <Waldman200036@gmail.com>
2020-06-18 20:02:55 -0500

    Books object added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import p from 'path';
 
 const app = express();
 const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
 const dbug = debug('app');
 const path = p;
 const dirname = path.resolve();
@@ -18,9 +19,54 @@ app.use('/js', express.static(path.join(dirname, '/node_modules/bootstrap/dist/j
 app.use('/js', express.static(path.join(dirname, '/node_modules/bootstrap/jqery/dist')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+
+const books = [{
+  title: 'War and Peace',
+  genres: 'Historical Fiction',
+  author: 'Lev Nikolayevich Tolstoy'
+},
+{
+  title: 'Les Miserables',
+  genres: 'Historical Fiction',
+  author: 'Victor Hugo'
+},
+{
+  title: 'The Time Machine',
+  genres: 'Science Fiction',
+  author: 'H.G. Wells'
+}
+];
+
+bookRouter.route('/')
+  .get((req, res) => {
+    res.render('books', {
+      nav: [{
+        link: '/Books',
+        title: 'Books'
+      }, {
+        link: '/authors',
+        title: 'Authors'
+      }],
+      title: 'Library',
+      books
+    });
+  });
+
+bookRouter.route('/single')
+  .get((req, res) => {
+    res.send('hello single book');
+  });
+
+app.use('/books', bookRouter);
 app.get('/', (_req, res) => {
   res.render('index', {
-    list: ['a', 'b'],
+    nav: [{
+      link: '/Books',
+      title: 'Books'
+    }, {
+      link: '/authors',
+      title: 'Authors'
+    }],
     title: 'Library',
   });
 });
