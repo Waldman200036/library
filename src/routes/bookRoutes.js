ea4c43766967dba03df4e3fe9e4f4b590c84a861
Waldman200036 <Waldman200036@gmail.com>
2020-06-21 21:11:22 -0500

    Final SQL Server Changes

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -26,41 +26,26 @@ function router(nav) {
       }());
     });
   bookRouter.route('/:id')
-    .get((req, res) => {
+    .all((req, res, next) => {
       (async function query() {
         const { id } = req.params;
         const request = new sql.Request();
         const { recordset } = await request.input('id', sql.Int, id)
           .query('select * from book where id = @id');
-        debug(recordset);
-        res.render(
-          'bookView',
-          {
-            nav,
-            title: 'Library',
-            book: recordset[0],
-          }
-        );
+        [req.book] = recordset;
+        next();
       }());
-    });
-  /*
-  bookRouter.route('/:id')
+    })
     .get((req, res) => {
-      (async function query() {
-        const { id } = req.params;
-        const request = new sql.Request();
-        const { recordset } = await request.input('id', sql.Int, id)
-          .query('select * from book where id = @id');
-        res.render(
-          'bookView',
-          {
-            nav,
-            title: 'Library',
-            book: recordset[0],
-          }
-        );
-      });
-    }); */
+      res.render(
+        'bookView',
+        {
+          nav,
+          title: 'Library',
+          book: req.book,
+        }
+      );
+    });
 
   return bookRouter;
 }
